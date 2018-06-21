9c0818aa173354ffcb57d4c558313ec4a9bf331e
Melo <wellington.melo@accenture.com>
2018-06-21 14:22:10 -0300

    Estamos no penultimo módulo

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () =>{
-    it('should say Minha Primeira Página Bootstrap', (done) => {
+    it('should say Users', (done) => {
         const index = fs.readFileSync('./src/index.html', "utf-8");
         jsdom.env(index, function(err, window){
             const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Minha Primeira Página Bootstrap");
+            expect(h1.innerHTML).to.equal("Users");
             done();
             window.close();
         });
